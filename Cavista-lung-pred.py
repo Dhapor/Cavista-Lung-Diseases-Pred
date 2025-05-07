1d0c00e0876e5f42b20fd78c1a64607423360be7
Oludare Samuel <138253128+Dhapor@users.noreply.github.com>
2025-05-07 17:06:12 +0100

    Update Cavista-lung-pred.py

--- Cavista-lung-pred.py
+++ Cavista-lung-pred.py
@@ -80,9 +80,9 @@ selected_page = st.sidebar.radio('Navigation', ['Home', 'Modeling'])
 
 def HomePage():
     # Streamlit app header
-    st.markdown("<h1 style = 'color: #2B2A4C; text-align: center; font-family:montserrat'>Lung Cancer Prediction Model</h1>",unsafe_allow_html=True)
+    st.markdown("<h1 style = ' text-align: center; font-family:montserrat'>Lung Cancer Prediction Model</h1>",unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
-    st.markdown("<h6 style = 'margin: -15px; color: #2B2A4C; text-align: center ; font-family:montserrat'>Lung cancer is one of the most common and deadliest forms of cancer worldwide. Early detection and intervention are crucial for improving patient outcomes. This project aims to develop a machine learning model that predicts the likelihood of an individual having lung cancer based on various health-related factors.</h6>",unsafe_allow_html=True)
+    st.markdown("<h6 style = 'margin: -15px;  text-align: center ; font-family:montserrat'>Lung cancer is one of the most common and deadliest forms of cancer worldwide. Early detection and intervention are crucial for improving patient outcomes. This project aims to develop a machine learning model that predicts the likelihood of an individual having lung cancer based on various health-related factors.</h6>",unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
     st.image('anatomical-images-human-lungs.jpg',  width = 700)
     st.markdown('<br>', unsafe_allow_html= True)
@@ -91,74 +91,74 @@ def HomePage():
 
     st.markdown('<br>', unsafe_allow_html= True)
     st.markdown('<br>', unsafe_allow_html= True)
-    st.markdown("<h3 style='color: #2B2A4C;text-align: center; font-family:montserrat'>The Model Features</h3>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center; font-family:montserrat'>The Model Features</h3>", unsafe_allow_html=True)
   
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Fatigue</h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; font-family:montserrat'>Fatigue</h3>", unsafe_allow_html=True)
     st.markdown("<p>Feeling unusually tired or weak, which can be a symptom of advanced lung cancer or a side effect of cancer treatment.There are three</p>", unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
     st.markdown('<br>', unsafe_allow_html= True)
 
   
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Age</h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; font-family:montserrat'>Age</h3>", unsafe_allow_html=True)
     st.markdown("<p>The Age field in the Heart Diseases Model denotes the individual's chronological age, a key factor in assessing heart disease risk. Age ranges from 28-77 in our dataset.</p>", unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
     st.markdown('<br>', unsafe_allow_html= True)
 
 
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Chronic Disease</h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; font-family:montserrat'>Chronic Disease</h3>", unsafe_allow_html=True)
     st.markdown("<p>CHRONIC DISEASE is a type of fat that is found in your blood. It's essential for building cells and making certain hormones, but having too much CHRONIC DISEASE can be harmful, especially for your heart. Levels between 100-129 mg/dL are considered near optimal, while Levels above 160 mg/dL are considered high.</p>", unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
     st.markdown('<br>', unsafe_allow_html= True)
   
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'> Shortness of Breath </h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; font-family:montserrat'> Shortness of Breath </h3>", unsafe_allow_html=True)
     st.markdown("<p>Difficulty breathing or feeling breathless, which can occur as lung tumors grow and obstruct airways.</p>", unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
     st.markdown('<br>', unsafe_allow_html= True)
   
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Chest pain</h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; font-family:montserrat'>Chest pain</h3>", unsafe_allow_html=True)
     st.markdown("<p>Chest pain or discomfort caused when your heart muscle doesn't get enough oxygen-rich blood.</p>", unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
     st.markdown('<br>', unsafe_allow_html= True)
   
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Allergy</h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; font-family:montserrat'>Allergy</h3>", unsafe_allow_html=True)
     st.markdown("<p>Resting blood pressure refers to the measurement of blood pressure when the body is at rest, typically in a seated or lying position and after a period of relaxation. Normal resting blood pressure typically falls below 120/80 mmHg</p>", unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
     st.markdown('<br>', unsafe_allow_html= True)
   
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Gender</h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; font-family:montserrat'>Gender</h3>", unsafe_allow_html=True)
     st.markdown("<p>Lung cancer incidence and mortality rates are higher in men than in women, although the gap has been narrowing in recent years due to changing smoking patterns.</p>", unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
     st.markdown('<br>', unsafe_allow_html= True)
   
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Yellow Fingers</h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; font-family:montserrat'>Yellow Fingers</h3>", unsafe_allow_html=True)
     st.markdown("<p>Nicotine in cigarette smoke can stain fingers and nails yellow, which can be a visible sign of smoking, a major risk factor for lung cancer.</p>", unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
     st.markdown('<br>', unsafe_allow_html= True)
   
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Smoking</h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; font-family:montserrat'>Smoking</h3>", unsafe_allow_html=True)
     st.markdown("<p>The single most significant risk factor for lung cancer. Smoking tobacco, including cigarettes, cigars, and pipes, greatly increases the risk of developing lung cancer.</p>", unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
     st.markdown('<br>', unsafe_allow_html= True)
 
 
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Alcohol Consuming</h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; font-family:montserrat'>Alcohol Consuming</h3>", unsafe_allow_html=True)
     st.markdown("<p>Heavy alcohol consumption, particularly when combined with smoking, can further increase the risk of developing lung cancer.</p>", unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
     st.markdown('<br>', unsafe_allow_html= True)
 
 
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Coughing</h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; font-family:montserrat'>Coughing</h3>", unsafe_allow_html=True)
     st.markdown("<p>Persistent cough, often with blood or bloody sputum, is a common symptom of lung cancer, especially in its early stages.</p>", unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
     st.markdown('<br>', unsafe_allow_html= True)
 
 
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Swallowing Difficulty</h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; font-family:montserrat'>Swallowing Difficulty</h3>", unsafe_allow_html=True)
     st.markdown("<p>People who have a hard time swallowing may choke on their food or liquid when trying to swallow. Dysphagia is the medical name for difficulty swallowing.</p>", unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
     st.markdown('<br>', unsafe_allow_html= True)
 
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Anxiety</h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: left; font-family:montserrat'>Anxiety</h3>", unsafe_allow_html=True)
     st.markdown("<p>Anxiety is an emotion characterized by feelings of tension, worried thoughts, and physical changes like increased blood pressure.</p>", unsafe_allow_html=True)
     st.markdown('<br>', unsafe_allow_html= True)
     st.markdown('<br>', unsafe_allow_html= True)
@@ -170,8 +170,8 @@ def HomePage():
 
 # Function to define the modeling page content
 def modeling_page():
-    st.markdown("<h1 style='text-align: CENTER; color: #2B2A4C;'>Modelling Section </h1>", unsafe_allow_html=True)
-    st.markdown("<h1 style='text-align: LEFT; color: #2B2A4C;'>Dataset Sample</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: CENTER; '>Modelling Section </h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: LEFT; '>Dataset Sample</h1>", unsafe_allow_html=True)
     # st.sidebar.markdown('<br><br><br>', unsafe_allow_html= True)
     st.write(ds[['AGE', 'FATIGUE', 'CHRONIC DISEASE', 'SHORTNESS OF BREATH', 'YELLOW_FINGERS', 
                     'SMOKING', 'GENDER', 'ALCOHOL CONSUMING', 'CHEST PAIN', 'COUGHING', 
@@ -230,7 +230,7 @@ if selected_page == "Modeling":
 
     if patient_name:
             if st.button('Press To Predict'):
-                st.markdown("<h4 style='color: #2B2A4C; text-align: left; font-family: montserrat;'>Model Report</h4>", unsafe_allow_html=True)
+                st.markdown("<h4 style=' text-align: left; font-family: montserrat;'>Model Report</h4>", unsafe_allow_html=True)
                 predicted = model.predict(input_variables)
                 st.toast('Predicted Successfully')
                 st.image('check icon.png', width=100)
@@ -243,4 +243,4 @@ if selected_page == "Modeling":
     
     st.markdown('<hr>', unsafe_allow_html=True)
     
-    st.markdown("<h8 style = 'color: #2B2A4C; text-align: LEFT; font-family:montserrat'>Lung Cancer Prediction MODEL BUILT BY The Orpheus Snipers at the Cavista Hackathon 2024</h8>",unsafe_allow_html=True)
+    st.markdown("<h8 style = ' text-align: LEFT; font-family:montserrat'>Lung Cancer Prediction MODEL BUILT BY The Orpheus Snipers at the Cavista Hackathon 2024</h8>",unsafe_allow_html=True)
